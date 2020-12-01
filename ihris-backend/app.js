1b74555f12a01404ac49fb13f82bd3374a88d88e
Nobert Mijumbi <nobertmn@gmail.com>
2020-11-30 23:37:19 -0800

    Added in security metadata for practitioner related resources and location hierarchies. So any Location or Practitioner, PractitionerRole, or Basic resources linked to Practitioners will have the practitioner associated with the resource in the security metadata as well as the full hierarchy of related Locations.

--- ihris-backend/app.js
+++ ihris-backend/app.js
@@ -125,12 +125,12 @@ const fs = require('fs')
   //const indexRouter = require('./routes/index')
   const configRouter = require('./routes/config')
   const authRouter = require('./routes/auth')
-  const fhirRouter = require('./routes/fhir')
   const esRouter = require('./routes/es')
   const questionnaireRouter = require('./routes/questionnaire')
+  const fhirRouter = require('./routes/fhir')
   const mheroRouter = require('./routes/mhero')
 
-  const limit = nconf.get("express:limit") || "200kb"
+  const limit = nconf.get("express:limit") || "50mb"
   app.use(express.json({
     type: ["application/json", "application/fhir+json"],
     limit: limit
